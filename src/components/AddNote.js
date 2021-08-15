d1689501fad8f79038435679611d7b474e383027
mahy209 <60543525+mahy209@users.noreply.github.com>
2021-08-15 04:01:00 +0200

    dropdown menu

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -1,51 +1,74 @@
 import { useState } from 'react';
-import { IoCheckmarkDoneOutline } from 'react-icons/io5'
+import 'antd/dist/antd.css';
+import { Menu, Dropdown, Upload } from 'antd';
+import { EllipsisOutlined, UploadOutlined, TagOutlined, CheckCircleOutlined } from '@ant-design/icons';
 
 const AddNote = ({ handleAddNote }) => {
+	const [noteTitle, setNoteTitle] = useState('');
 	const [noteText, setNoteText] = useState('');
+
 	const characterLimit = 200;
 
-	const handleChange = (event) => {
+	const handleTextChange = (event) => {
 		if (characterLimit - event.target.value.length >= 0) {
 			setNoteText(event.target.value);
 		}
 	};
 
-	
-	const [noteTitle, setUpdateTitle] = useState('');
-	const handleUpdateTitle = (event) => {
-		const title = event.target.value;
-		setUpdateTitle(title, noteTitle.title, "title");
+	const handleTitleChange = (event) => {
+		setNoteTitle(event.target.value);
 	};
 
-	const handleSaveClick = () => {
-		if (noteText.trim().length > 0) {
-			handleAddNote(noteText);
+	const handleSaveClick = (event) => {
+		if (noteText.trim().length > 0 && noteTitle.trim().length > 0) {
+			handleAddNote(noteText,noteTitle);
 			setNoteText('');
+			setNoteTitle('')
 		}
 	};
+	
+
+	  const menu = (
+		<Menu className="dropdown-menu">
+		  <Menu.Item>
+			  <Upload
+      action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+      listType="picture"
+      className="upload-list-inline"
+	  maxCount={1}> <UploadOutlined />
+    </Upload> </Menu.Item>
+		  <Menu.Item> <TagOutlined /> </Menu.Item>
+		</Menu>
+	  );
 
 	return (
 		<div className='note new'>
+			<div className='note-nav'>
 			<input
                 className="note-title"
                 type="text"
                 placeholder="Title"
                 value={noteTitle}
-                onChange={handleUpdateTitle}
+                onChange={handleTitleChange}
             />
+                <Dropdown overlay={menu} placement="bottomCenter" trigger={['click']}>
+				<a className="ant-dropdown-link" href="/#" onClick={e => e.preventDefault()}>
+					<EllipsisOutlined />
+                </a>
+            </Dropdown>
+			</div>
 			<textarea
 				rows='8'
 				cols='10'
 				placeholder='Type to add a note...'
 				value={noteText}
-				onChange={handleChange}
+				onChange={handleTextChange}
 			></textarea>
 			<div className='note-footer'>
 				<small>
 					{characterLimit - noteText.length} Remaining
 				</small>
-				<IoCheckmarkDoneOutline className='save' size='1.3em' onClick={handleSaveClick}/>
+				<CheckCircleOutlined className='save' size='1.3em' onClick={handleSaveClick}/>
 			</div>
 		</div>
 	);
